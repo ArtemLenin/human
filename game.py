a6da12e9daab1d6d0879c05e494d0c00b5eed72a
artemida <artemlenin96@gmail.com>
2022-08-02 20:15:34 +0500

    Revert "adsasdasd"
    This reverts commit d55ed7a52614935a10102416c1ab624dfe2bca05.

--- game.py
+++ game.py
@@ -3,4 +3,13 @@ user_lifes = 3
 while user_lifes > 0:
     user_lifes -= 1
     user_number = int(input("Введите число: "))
-    if 
+    if user_number == comp_number:
+        print("Win!")
+        break
+    elif user_number > comp_number:
+        print("Моё число меньше!")
+    elif user_number < comp_number:
+        print("Моё число больше!")
+else:
+    print("Lose!")
+print("Game Over")
